d7df9b19efbd6011367dec99a1ae944628c95ac7
Jibran <jibrannaeem4855@gmail.com>
2024-03-08 15:43:23 +0500

    adsense body tag

--- src/components/General4.js
+++ src/components/General4.js
@@ -1,30 +1,40 @@
 import React, { useEffect } from "react";
-import { Helmet } from "react-helmet";
 import portfolio2 from "../assets/images/s4.jpg";
-
 const General4 = () => {
   useEffect(() => {
+    // Create and append the AdSense script element
     const script = document.createElement("script");
     script.src =
       "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1058320936351991";
     script.async = true;
     script.crossOrigin = "anonymous";
-    document.head.appendChild(script);
+    document.body.appendChild(script);
 
+    // Create and append the AdSense ins element
+    const ins = document.createElement("ins");
+    ins.className = "adsbygoogle";
+    ins.style.display = "block";
+    ins.setAttribute("data-ad-client", "ca-pub-1058320936351991");
+    ins.setAttribute("data-ad-slot", "6828621820");
+    ins.setAttribute("data-ad-format", "auto");
+    ins.setAttribute("data-full-width-responsive", "true");
+    document.body.appendChild(ins);
+
+    // Push the AdSense script
+    const pushScript = document.createElement("script");
+    pushScript.text = "(adsbygoogle = window.adsbygoogle || []).push({});";
+    document.body.appendChild(pushScript);
+
+    // Clean up function to remove added elements
     return () => {
-      document.head.removeChild(script);
+      document.body.removeChild(script);
+      document.body.removeChild(ins);
+      document.body.removeChild(pushScript);
     };
   }, []);
 
   return (
     <>
-      <Helmet>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1058320936351991"
-          crossOrigin="anonymous"
-        />
-      </Helmet>
       <div style={{ backgroundColor: "#f9f5f2", padding: "30px" }}>
         <h1 style={{ color: "black" }}>Computerized Tuning</h1>
         <p style={{ color: "black", fontSize: "45px", textAlign: "justify" }}>
